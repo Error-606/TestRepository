87dc8fa9e62d44d427f9a03a4dbc814ba6dcf942
Error-606 <65509864+Error-606@users.noreply.github.com>
2020-10-25 11:18:37 -0600

    Added a new function

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+def add(*argv):
+  sum = 0 
+  for arg in argv:
+    sum += arg
+  return sum
+
 def main():
-  print("Hello World")
+  add(67, 86, 86, 90, 39) 
   return 0
   
 if __name__ == "__main__":
